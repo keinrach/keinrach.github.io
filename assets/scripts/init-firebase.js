c809b25f4464e827d99d303f991b968691721a95
keinrach <158367067+keinrach@users.noreply.github.com>
2024-04-24 15:39:32 -0700

    fixed sisu issues

--- assets/scripts/init-firebase.js
+++ assets/scripts/init-firebase.js
@@ -14,52 +14,5 @@ const auth = firebase.auth();
 //auth.useEmulator("http://127.0.0.1:9099");
 
 
-// signup
-const signupForm = document.querySelector("#newUserSignUpForm");
-if(signupForm) {
-  signupForm.addEventListener('submit', (e) =>{
-    e.preventDefault();
-    
-    // get user info
-    const email = signupForm['signUpEmail'].value;
-    const password = signupForm['signUpPassword'].value;
-    const confirmPassword = signupForm['signUpConfirmPassword'].value;
-
-    // sign up the user
-    auth.createUserWithEmailAndPassword(email, password).then(cred => {
-      const user = cred.user;
-      console.log("User signed up:", user);
-    }).then(() => {
-        // const modal = document.querySelector('#modal-signup');
-        // M.Modal.getInstance(modal).close();
-        signupForm.reset();
-        signupForm.querySelector('.error').innerHTML = 'Successfully created your account, please log-in.';
-    }).catch(err => {
-        signupForm.querySelector('.error').innerHTML = err.message;
-    });
-  });
-}
 
 
-//login
-const loginForm = document.querySelector("#signInForm");
-if(loginForm){
-  console.log("loginForm is not empty");
-  loginForm.addEventListener('submit', (e) =>{
-    e.preventDefault();
-    
-    //get user info
-    const email = loginForm['signInEmail'].value;
-    console.log("login form has email", email);
-    const password = loginForm['signInPassword'].value;
-
-    auth.signInWithEmailAndPassword(email, password).then(cred => {
-        console.log(cred.user);
-        loginForm.reset();
-        loginForm.querySelector('.error').innerHTML = '';
-        location.replace("https://bokchoyinvest.com")
-    }).catch(err => {
-        loginForm.querySelector('.error').innerHTML = err.message;
-    });
-});
-}
